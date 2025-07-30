fdd6b4b68af677089f241f5db634e063e0326042
CodeAlligator <codealligator.csprasad@gmail.com>
2025-07-30 12:25:39 +0530

    test(ci): file entry

--- Sources/ArchiveDetective/JSONFetcher.swift
+++ Sources/ArchiveDetective/JSONFetcher.swift
@@ -4,6 +4,7 @@ enum JSONFetcher {
     static let jsonURL = URL(string: "https://raw.githubusercontent.com/dkhamsing/open-source-ios-apps/master/contents.json")!
 
     static func fetch() async throws -> [String: Any] {
+        print("Entered JsonFetcher\(jsonURL)")
         let (data, response) = try await URLSession.shared.data(from: jsonURL)
         
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
